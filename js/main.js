736aee2eadb06381f12d44cd1126fa037ede2df2
angelabenavente <angela.b.almazan@gmail.com>
2019-11-20 18:37:56 +0100

    Improvements

--- js/main.js
+++ js/main.js
@@ -2,33 +2,37 @@
 
 const numberChoosenNode = document.querySelector('.number');
 const submitButtonNode = document.querySelector('.submitButton');
-const clueTextNode = document.querySelector('.clueDiv > .clue');
+const clueTextNode = document.querySelector('.clueDiv > .clueText');
 const attempsTextNode = document.querySelector('.attempsDiv > .attemps > .attempsNumber');
+const maxNumber = 100;
+const minNumber = 0;
+let tryCounter = 0;
 
-function getRandomNumber(max) {
-  return Math.ceil(Math.random() * max);
-}
 
-const randomNumber = getRandomNumber(100);
+const getRandomNumber = max => Math.ceil(Math.random() * max);
+const randomNumber = getRandomNumber(maxNumber);
 console.log(`Mi número aleatorio es: ${randomNumber}.`);
 
 function increaseCountClicks() {
-  let tryCounter = parseInt(attempsTextNode.innerHTML);
-  tryCounter = tryCounter + 1;
+  attempsTextNode.innerHTML = tryCounter ;
+  tryCounter++;
   attempsTextNode.innerHTML = tryCounter;
 }
 
 function getClue() {
-  if (randomNumber === parseInt(numberChoosenNode.value)) {
-    clueTextNode.innerHTML = '¡¡¡Has ganado, campeona!!!.';
+  if (numberChoosenNode.value == '' || numberChoosenNode.value > maxNumber || numberChoosenNode.value <= minNumber || isNaN(numberChoosenNode.value) === false) {
+    clueTextNode.innerHTML = 'Debe introducir un número entre 1 y 100.';
+    //isNaN validation because I can write 'e' letter in spite of type number input.
   } else if (randomNumber > numberChoosenNode.value) {
     clueTextNode.innerHTML = 'Demadiado bajo.';
   } else if (randomNumber < numberChoosenNode.value) {
     clueTextNode.innerHTML = 'Demadiado alto.';
-  } else if (isNaN(numberChoosenNode.value) ) {
-    clueTextNode.innerHTML = 'Debe introducir un número.';
-  } else if (parseInt(numberChoosenNode.value) == null || numberChoosenNode.value.length == 0 || /^\s*$/.test(numberChoosenNode.value) || parseInt(numberChoosenNode.value) < 0 || parseInt(numberChoosenNode.value) > 100 ) { clueTextNode.innerHTML = 'Debe introducir un número entre 1 y 100.'; }
+  } else {
+    clueTextNode.innerHTML = '¡¡¡Has ganado, campeona!!!.';
+  }
   increaseCountClicks()
 }
-
-submitButtonNode.addEventListener('click', getClue);
+function playGame(){
+  getClue();
+}
+submitButtonNode.addEventListener('click', playGame);
